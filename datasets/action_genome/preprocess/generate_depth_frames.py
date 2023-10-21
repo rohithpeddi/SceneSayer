69843b79d77711e5bb7afd8d239e687d314bcc09
Rohith Peddi <rxp190007@utdallas.edu>
2023-10-21 14:56:58 -0500

    Depth estimation

--- datasets/action_genome/preprocess/generate_depth_frames.py
+++ datasets/action_genome/preprocess/generate_depth_frames.py
@@ -7,67 +7,50 @@ import matplotlib.pyplot as plt
 import torch
 import os
 import numpy as np
+from logger_config import get_logger
+
+logger = get_logger(__name__)
 
 
 class DepthEstimator:
-	def __init__(self, modelType="DPT_Large", imgs_dir="./test/", depth_dir="./output/", batch_size=1):
-		self.TEST_PATH = imgs_dir
-		self.IMAGE_SIZE = 384
-		self.BATCH_SIZE = batch_size
-		self.DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-		self.OUTPUT_PATH = depth_dir
-		os.makedirs(self.OUTPUT_PATH, exist_ok=True)
-		self.testTransform = Compose([
-			Resize((self.IMAGE_SIZE, self.IMAGE_SIZE)), ToTensor()])
-		self.midas = torch.hub.load("intel-isl/MiDaS", modelType)
-		self.midas.to(self.DEVICE)
+	def __init__(self, input_path, output_path, model_type="DPT_Large", batch_size=1):
+		self.batch_size = batch_size
+		self.device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+		
+		self.midas = torch.hub.load("intel-isl/MiDaS", model_type)
+		self.midas.to(self.device)
 		self.midas.eval()
-		# self.testDataset = ImageFolder(self.TEST_PATH)
-		# self.testLoader = DataLoader(self.testDataset, batch_size=self.BATCH_SIZE, shuffle=False)
-		if not os.path.exists(self.OUTPUT_PATH):
-			os.makedirs(self.OUTPUT_PATH)
-		self.idx = 0
+		
+		self.input_path = input_path
+		self.output_path = output_path
+		os.makedirs(self.output_path, exist_ok=True)
+		
 		midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-		if modelType == "DPT_Large" or modelType == "DPT_Hybrid":
+		if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
 			self.transform = midas_transforms.dpt_transform
 		else:
 			self.transform = midas_transforms.small_transform
 	
 	def run(self):
-		for image_name in os.listdir(self.TEST_PATH):
-			print("Processing: ", image_name)
-			image_path = os.path.join(self.TEST_PATH, image_name)
+		logger.info("----------------------------------------------------")
+		logger.info("Processing images files in input path: {}".format(self.input_path))
+		for image_name in os.listdir(self.input_path):
+			image_path = os.path.join(self.input_path, image_name)
 			img = cv2.imread(image_path)
 			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			self.process_images(img)
-	
-	def process_images(self, img):
-		images = self.transform(img).to(self.DEVICE)
-		with torch.no_grad():
-			prediction = self.midas(images)
-			prediction = torch.nn.functional.interpolate(
-				prediction.unsqueeze(1), size=img.shape[:2], mode="bicubic",
-				align_corners=False).squeeze()
-		# convert to a numpy array
-		if len(prediction.shape) == 2:
-			output = np.array([prediction.cpu().numpy()])
-		else:
+			
+			with torch.no_grad():
+				prediction = self.midas(img)
+				prediction = torch.nn.functional.interpolate(
+					prediction.unsqueeze(1), size=img.shape[:2], mode="bicubic",
+					align_corners=False).squeeze()
+			
 			output = prediction.cpu().numpy()
-		
-		axes = []
-		fig = plt.figure(figsize=(10, 5))
-		# loop over the rows and columns
-		for i in range(self.BATCH_SIZE):
-			axes.append(fig.add_subplot(1, 2, 1))
-			plt.imshow(images[i].permute((1, 2, 0)).cpu().detach().numpy())
-			axes.append(fig.add_subplot(1, 2, 2))
-			plt.imshow(output[i])
-			fig.tight_layout()
-			outputFileName = os.path.join(self.OUTPUT_PATH, f'{self.idx}.jpg')
-			plt.savefig(outputFileName)
-			self.idx += 1
+			plt.imsave(os.path.join(self.output_path, image_name), output)
+		logger.info("Finished processing images files in input path: {}".format(self.input_path))
 
 
 if __name__ == "__main__":
-	depth_estimator = DepthEstimator(imgs_dir="/data/rohith/ag/frames_annotated/0BX9N.mp4", depth_dir="/data/rohith/ag/depth/frames_annotated/0BX9N.mp4")
+	depth_estimator = DepthEstimator(input_path="/data/rohith/ag/frames_annotated/0BX9N.mp4",
+	                                 output_path="/data/rohith/ag/depth/frames_annotated/0BX9N.mp4")
 	depth_estimator.run()

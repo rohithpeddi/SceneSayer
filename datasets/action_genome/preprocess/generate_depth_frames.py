c1a8e202c6011dfb0636e6db920a669636768e78
rxp190007 <rxp190007@utdallas.edu>
2023-10-21 14:42:24 -0500

    From 94092

--- datasets/action_genome/preprocess/generate_depth_frames.py
+++ datasets/action_genome/preprocess/generate_depth_frames.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 from torchvision.transforms import Compose, ToTensor, Resize
 from torchvision.datasets import ImageFolder
@@ -15,27 +16,37 @@ class DepthEstimator:
 		self.BATCH_SIZE = batch_size
 		self.DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 		self.OUTPUT_PATH = depth_dir
+		os.makedirs(self.OUTPUT_PATH, exist_ok=True)
 		self.testTransform = Compose([
 			Resize((self.IMAGE_SIZE, self.IMAGE_SIZE)), ToTensor()])
 		self.midas = torch.hub.load("intel-isl/MiDaS", modelType)
 		self.midas.to(self.DEVICE)
 		self.midas.eval()
-		self.testDataset = ImageFolder(self.TEST_PATH, self.testTransform)
-		self.testLoader = DataLoader(self.testDataset, batch_size=self.BATCH_SIZE, shuffle=False)
+		# self.testDataset = ImageFolder(self.TEST_PATH)
+		# self.testLoader = DataLoader(self.testDataset, batch_size=self.BATCH_SIZE, shuffle=False)
 		if not os.path.exists(self.OUTPUT_PATH):
 			os.makedirs(self.OUTPUT_PATH)
 		self.idx = 0
+		midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+		if modelType == "DPT_Large" or modelType == "DPT_Hybrid":
+			self.transform = midas_transforms.dpt_transform
+		else:
+			self.transform = midas_transforms.small_transform
 	
 	def run(self):
-		for images, _ in self.testLoader:
-			self.process_images(images)
+		for image_name in os.listdir(self.TEST_PATH):
+			print("Processing: ", image_name)
+			image_path = os.path.join(self.TEST_PATH, image_name)
+			img = cv2.imread(image_path)
+			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			self.process_images(img)
 	
-	def process_images(self, images):
-		images = images.to(self.DEVICE)
+	def process_images(self, img):
+		images = self.transform(img).to(self.DEVICE)
 		with torch.no_grad():
 			prediction = self.midas(images)
 			prediction = torch.nn.functional.interpolate(
-				prediction.unsqueeze(1), size=[self.IMAGE_SIZE, self.IMAGE_SIZE], mode="bicubic",
+				prediction.unsqueeze(1), size=img.shape[:2], mode="bicubic",
 				align_corners=False).squeeze()
 		# convert to a numpy array
 		if len(prediction.shape) == 2:
@@ -52,12 +63,11 @@ class DepthEstimator:
 			axes.append(fig.add_subplot(1, 2, 2))
 			plt.imshow(output[i])
 			fig.tight_layout()
-			# self.testLoader.
 			outputFileName = os.path.join(self.OUTPUT_PATH, f'{self.idx}.jpg')
 			plt.savefig(outputFileName)
 			self.idx += 1
 
 
 if __name__ == "__main__":
-	depth_estimator = DepthEstimator(imgs_dir="./test/", depth_dir="./output/")
+	depth_estimator = DepthEstimator(imgs_dir="/data/rohith/ag/frames_annotated/0BX9N.mp4", depth_dir="/data/rohith/ag/depth/frames_annotated/0BX9N.mp4")
 	depth_estimator.run()

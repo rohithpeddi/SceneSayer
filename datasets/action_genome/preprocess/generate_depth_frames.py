01dbb87260324374ee5651ecd76a2983e3cab3d4
Rohith Peddi <rxp190007@utdallas.edu>
2023-10-21 21:24:28 -0500

    Updated depth and pose estimation

--- datasets/action_genome/preprocess/generate_depth_frames.py
+++ datasets/action_genome/preprocess/generate_depth_frames.py
@@ -3,14 +3,12 @@ import matplotlib.pyplot as plt
 import torch
 import logging
 import os
-import threading
-from concurrent.futures import ThreadPoolExecutor
 
 from tqdm import tqdm
 
 log_directory = os.path.join(os.getcwd(), 'logs')
 if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
+	os.makedirs(log_directory)
 
 log_file_path = os.path.join(log_directory, f"std.log")
 logging.basicConfig(filename=log_file_path, filemode='a', level=logging.INFO,
@@ -20,59 +18,74 @@ logger = logging.getLogger(__name__)
 
 
 class DepthEstimator:
-    def __init__(self, input_path, output_path, model_type="DPT_Large", batch_size=1):
-        self.batch_size = batch_size
-        self.device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-
-        self.midas = torch.hub.load("intel-isl/MiDaS", model_type)
-        self.midas.to(self.device)
-        self.midas.eval()
-
-        self.input_path = input_path
-        self.output_path = output_path
-        os.makedirs(self.output_path, exist_ok=True)
-
-        midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-        if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
-            self.transform = midas_transforms.dpt_transform
-        else:
-            self.transform = midas_transforms.small_transform
-
-    def run(self):
-        logger.info("Processing images files in input path: {}".format(self.input_path))
-        try:
-            for image_name in os.listdir(self.input_path):
-                image_path = os.path.join(self.input_path, image_name)
-                img = cv2.imread(image_path)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-                input_batch = self.transform(img).to(self.device)
-                with torch.no_grad():
-                    prediction = self.midas(input_batch)
-                    prediction = torch.nn.functional.interpolate(
-                        prediction.unsqueeze(1), size=img.shape[:2], mode="bicubic",
-                        align_corners=False).squeeze()
-
-                output = prediction.cpu().numpy()
-                plt.imsave(os.path.join(self.output_path, image_name), output)
-            logger.info("Finished processing images files in input path: {}".format(self.input_path))
-        except Exception as e:
-            logger.error("Error processing images files in input path: {}".format(self.input_path))
-            logger.error(e)
-            return
-        return
-
-
-def estimate_monocular_depth(input_path, output_path):
-    def process_directory(frame_directory_name):
-        in_path = os.path.join(input_path, frame_directory_name)
-        out_path = os.path.join(output_path, frame_directory_name)
-        depth_estimator = DepthEstimator(input_path=in_path, output_path=out_path)
-        depth_estimator.run()
-
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        executor.map(process_directory, os.listdir(input_path))
+	def __init__(self, model_type="DPT_Large", batch_size=1):
+		self.batch_size = batch_size
+		self.device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+		
+		self.midas = torch.hub.load("intel-isl/MiDaS", model_type)
+		self.midas.to(self.device)
+		self.midas.eval()
+		
+		midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+		if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+			self.transform = midas_transforms.dpt_transform
+		else:
+			self.transform = midas_transforms.small_transform
+	
+	def process_image_batch(self, image_batch):
+		input_batch = self.transform(image_batch).to(self.device)
+		with torch.no_grad():
+			prediction = self.midas(input_batch)
+			prediction = torch.nn.functional.interpolate(
+				prediction.unsqueeze(1), size=image_batch[0].shape[:2], mode="bicubic",
+				align_corners=False).squeeze()
+		return prediction
+	
+	def process_video_directory(self, input_path, output_path):
+		logger.info("Processing images files in input path: {}".format(input_path))
+		try:
+			# Set the batch size
+			batch_size = 10
+			
+			# Loop through image files in the input_path
+			for i in range(0, len(os.listdir(input_path)), batch_size):
+				batch_images = []
+				batch_names = []
+				
+				# Load and preprocess images for the batch
+				for image_name in os.listdir(input_path)[i:i + batch_size]:
+					image_path = os.path.join(input_path, image_name)
+					img = cv2.imread(image_path)
+					img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+					input_tensor = self.transform(img).to(self.device)
+					batch_images.append(input_tensor)
+					batch_names.append(image_name)
+				
+				# Stack the batch of images
+				batch_images = torch.stack(batch_images)
+				
+				# Perform predictions for the batch
+				batch_prediction = self.process_image_batch(batch_images)
+				
+				# Save the batch of predictions
+				for j in range(len(batch_names)):
+					output = batch_prediction[j].cpu().numpy()
+					plt.imsave(os.path.join(output_path, batch_names[j]), output)
+			
+			logger.info("Finished processing images files in input path: {}".format(input_path))
+		except Exception as e:
+			logger.error("Error processing images files in input path: {}".format(input_path))
+			logger.error(e)
+			return
+		return
+	
+	def process_videos(self, input_path, output_path):
+		for video_directory_name in tqdm(os.listdir(input_path), desc="Processing videos"):
+			video_input_path = os.path.join(input_path, video_directory_name)
+			video_output_path = os.path.join(output_path, video_directory_name)
+			self.process_video_directory(video_input_path, video_output_path)
 
 
 if __name__ == "__main__":
-    estimate_monocular_depth("/data/rohith/ag/frames", "/data/rohith/ag/depth/frames")
+	depth_estimator = DepthEstimator()
+	depth_estimator.process_videos("/data/rohith/ag/frames", "/data/rohith/ag/depth/frames")

9dd908a8622b021967a071a784f2f7fbb309b895
saksham002 <77882525+saksham002@users.noreply.github.com>
2023-12-27 20:08:53 +0530

    Update test_ODE.py

--- test_ODE.py
+++ test_ODE.py
@@ -25,6 +25,7 @@ def test_ode():
 	          max_window=max_window).to(device=gpu_device)
 	
 	ode.eval()
+
 	# cttran.eval()
 	
 	ckpt = torch.load(conf.model_path, map_location=gpu_device)
@@ -49,9 +50,17 @@ def test_ode():
 			pred = ode(entry, True)
 			vid_no = gt_annotation[0][0]["frame"].split('.')[0]
 			all_time.append(time() - start)
-			for i in range(1, max_window + 1):
+			w = max_window
+			if max_window == -1:
+				w = len(gt_annotation) - 1
+			global_output = pred["global_output"]
+			times = pred["times"]
+			global_output_mod = global_output.clone().to(global_output.device)
+			denominator = torch.zeros(global_output.size(0)).to(global_output.device) + 1.0
+			for i in range(1, w + 1):
 				pred_anticipated = pred.copy()
 				mask_curr = pred["mask_curr_" + str(i)]
+				mask_gt = pred["mask_gt_" + str(i)]
 				pred_anticipated["spatial_distribution"] = pred["anticipated_spatial_distribution"][i - 1][mask_curr]
 				pred_anticipated["contacting_distribution"] = pred["anticipated_contacting_distribution"][i - 1][
 					mask_curr]
@@ -67,22 +76,38 @@ def test_ode():
 					pred_anticipated["pred_labels"] = pred["pred_labels_test_" + str(i)]
 				pred_anticipated["boxes"] = pred["boxes_test_" + str(i)]
 				if conf.modified_gt:
-					with_constraint_evaluator.evaluate_scene_graph(entry["gt_annotation_" + str(i)][i:], pred_anticipated)
-					no_constraint_evaluator.evaluate_scene_graph(entry["gt_annotation_" + str(i)][i:], pred_anticipated)
+					with_constraint_evaluator.evaluate_scene_graph(pred["gt_annotation_" + str(i)][i:], pred_anticipated)
+					no_constraint_evaluator.evaluate_scene_graph(pred["gt_annotation_" + str(i)][i:], pred_anticipated)
 				else:
-					with_constraint_evaluator.evaluate_scene_graph(entry["gt_annotation"][i:], pred_anticipated)
-					no_constraint_evaluator.evaluate_scene_graph(entry["gt_annotation"][i:], pred_anticipated)
-	
+					with_constraint_evaluator.evaluate_scene_graph(gt_annotation[i:], pred_anticipated)
+					no_constraint_evaluator.evaluate_scene_graph(gt_annotation[i:], pred_anticipated)
+				global_output_mod[mask_gt] += pred["anticipated_vals"][i - 1][mask_curr] / torch.reshape((times[mask_gt] - times[mask_curr] + 1), (-1, 1))
+				denominator[mask_gt] += 1 / (times[mask_gt] - times[mask_curr] + 1)
+			global_output_mod = global_output_mod / torch.reshape(denominator, (-1, 1))
+			pred["global_output"] = global_output_mod
+			pred["attention_distribution"] = ode.dsgdetr.a_rel_compress(global_output)
+			pred["spatial_distribution"] = ode.dsgdetr.s_rel_compress(global_output)
+			pred["contacting_distribution"] = ode.dsgdetr.c_rel_compress(global_output)
+			pred["spatial_distribution"] = torch.sigmoid(pred["spatial_distribution"])
+			pred["contacting_distribution"] = torch.sigmoid(pred["contacting_distribution"])
+			with_constraint_evaluator_gen.evaluate_scene_graph(gt_annotation, pred)
+			no_constraint_evaluator_gen.evaluate_scene_graph(gt_annotation, pred)
 	print('Average inference time', np.mean(all_time))
 	
+	print("anticipation evaluation:")
 	print('-------------------------with constraint-------------------------------')
 	with_constraint_evaluator.print_stats()
 	print('-------------------------no constraint-------------------------------')
 	no_constraint_evaluator.print_stats()
-
+	print("generation evaluation")
+	print('-------------------------with constraint-------------------------------')
+	with_constraint_evaluator_gen.print_stats()
+	print('-------------------------no constraint-------------------------------')
+	no_constraint_evaluator_gen.print_stats()
 
 if __name__ == '__main__':
 	ag_features_test, dataloader_test, with_constraint_evaluator, no_constraint_evaluator, semi_constraint_evaluator, gpu_device, conf = fetch_test_basic_config()
+	x, y, with_constraint_evaluator_gen, no_constraint_evaluator_gen, semi_constraint_evaluator_gen, z, w = fetch_test_basic_config()  
 	test_ode()
 
 #  python test_cttran.py -mode sgdet -datasize large -data_path /home/cse/msr/csy227518/scratch/Datasets/action_genome/ -model_sttran_path cttran/no_temporal/sttran_9.tar -model_cttran_path cttran/no_temporal/cttran_9.tar

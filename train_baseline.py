2fbed3fe497da1ce01e1f6523aa51c508fb7122a
Rohith Peddi <rxp190007@utdallas.edu>
2024-01-08 04:24:38 -0600

    Changes for training baseline with anticipation

--- train_baseline.py
+++ train_baseline.py
@@ -32,103 +32,100 @@ def process_train_video(conf, entry, optimizer, model, epoch, num, tr, gpu_devic
 
     context = min(context, total_frames - 1)
     future = min(future, total_frames - context)
-
+    
     while start + context + 1 <= total_frames:
         future_frame_start_id = entry["im_idx"].unique()[context]
-
+        
         if start + context + future > total_frames > start + context:
             future = total_frames - (start + context)
-
+        
         future_frame_end_id = entry["im_idx"].unique()[context + future - 1]
-
+        
         context_end_idx = int(torch.where(entry["im_idx"] == future_frame_start_id)[0][0])
-        context_idx = entry["im_idx"][:context_end_idx]
-        context_len = context_idx.shape[0]
-
         future_end_idx = int(torch.where(entry["im_idx"] == future_frame_end_id)[0][-1]) + 1
-        future_idx = entry["im_idx"][context_end_idx:future_end_idx]
-        future_len = future_idx.shape[0]
-
-        attention_distribution = pred["output"][count]["attention_distribution"]
-        spatial_distribution = pred["output"][count]["spatial_distribution"]
-        contact_distribution = pred["output"][count]["contacting_distribution"]
-
-        attention_label = torch.tensor(pred["attention_gt"][context_end_idx:future_end_idx], dtype=torch.long).to(
-            device=attention_distribution.device).squeeze()
-
-        if not conf.bce_loss:
-            spatial_label = -torch.ones([len(pred["spatial_gt"][context_end_idx:future_end_idx]), 6],
-                                        dtype=torch.long).to(device=attention_distribution.device)
-            contact_label = -torch.ones([len(pred["contacting_gt"][context_end_idx:future_end_idx]), 17],
-                                        dtype=torch.long).to(device=attention_distribution.device)
-            for i in range(len(pred["spatial_gt"][context_end_idx:future_end_idx])):
-                spatial_label[i, : len(pred["spatial_gt"][context_end_idx:future_end_idx][i])] = torch.tensor(
-                    pred["spatial_gt"][context_end_idx:future_end_idx][i])
-                contact_label[i,
-                : len(pred["contacting_gt"][context_end_idx:future_end_idx][i])] = torch.tensor(
-                    pred["contacting_gt"][context_end_idx:future_end_idx][i])
+        if pred["output"][count]["scatter_flag"] == 0:
+            attention_distribution = pred["output"][count]["attention_distribution"]
+            spatial_distribution = pred["output"][count]["spatial_distribution"]
+            contact_distribution = pred["output"][count]["contacting_distribution"]
+            
+            attention_label = torch.tensor(pred["attention_gt"][context_end_idx:future_end_idx], dtype=torch.long).to(
+                device=attention_distribution.device).squeeze()
+            
+            if not conf.bce_loss:
+                spatial_label = -torch.ones([len(pred["spatial_gt"][context_end_idx:future_end_idx]), 6],
+                                            dtype=torch.long).to(device=attention_distribution.device)
+                contact_label = -torch.ones([len(pred["contacting_gt"][context_end_idx:future_end_idx]), 17],
+                                            dtype=torch.long).to(device=attention_distribution.device)
+                for i in range(len(pred["spatial_gt"][context_end_idx:future_end_idx])):
+                    spatial_label[i, : len(pred["spatial_gt"][context_end_idx:future_end_idx][i])] = torch.tensor(
+                        pred["spatial_gt"][context_end_idx:future_end_idx][i])
+                    contact_label[i, : len(pred["contacting_gt"][context_end_idx:future_end_idx][i])] = torch.tensor(
+                        pred["contacting_gt"][context_end_idx:future_end_idx][i])
+            else:
+                spatial_label = torch.zeros([len(pred["spatial_gt"][context_end_idx:future_end_idx]), 6],
+                                            dtype=torch.float32).to(device=attention_distribution.device)
+                contact_label = torch.zeros([len(pred["contacting_gt"][context_end_idx:future_end_idx]), 17],
+                                            dtype=torch.float32).to(device=attention_distribution.device)
+                for i in range(len(pred["spatial_gt"][context_end_idx:future_end_idx])):
+                    spatial_label[i, pred["spatial_gt"][context_end_idx:future_end_idx][i]] = 1
+                    contact_label[i, pred["contacting_gt"][context_end_idx:future_end_idx][i]] = 1
+            
+            context_boxes_idx = torch.where(entry["boxes"][:, 0] == context)[0][0]
+            context_excluding_last_frame_boxes_idx = torch.where(entry["boxes"][:, 0] == context - 1)[0][0]
+            future_boxes_idx = torch.where(entry["boxes"][:, 0] == context + future - 1)[0][-1]
+            
+            if conf.mode == 'predcls':
+                context_last_frame_labels = set(
+                    pred["labels"][context_excluding_last_frame_boxes_idx:context_boxes_idx].tolist())
+                future_labels = set(pred["labels"][context_boxes_idx:future_boxes_idx + 1].tolist())
+                context_labels = set(pred["labels"][:context_boxes_idx].tolist())
+            else:
+                context_last_frame_labels = set(
+                    pred["pred_labels"][context_excluding_last_frame_boxes_idx:context_boxes_idx].tolist())
+                future_labels = set(pred["pred_labels"][context_boxes_idx:future_boxes_idx + 1].tolist())
+                context_labels = set(pred["pred_labels"][:context_boxes_idx].tolist())
+            
+            appearing_object_labels = future_labels - context_last_frame_labels
+            disappearing_object_labels = context_labels - context_last_frame_labels
+            ignored_object_labels = appearing_object_labels.union(disappearing_object_labels)
+            ignored_object_labels = list(ignored_object_labels)
+            
+            # Weighting loss based on appearance or disappearance of objects
+            # We only consider loss on objects that are present in the last frame of the context
+            weight = torch.ones(pred["output"][count]["global_output"].shape[0]).cuda()
+            for object_label in ignored_object_labels:
+                for idx, pair in enumerate(pred["pair_idx"][context_end_idx:future_end_idx]):
+                    if conf.mode == 'predcls':
+                        if pred["labels"][pair[1]] == object_label:
+                            weight[idx] = 0
+                    else:
+                        if pred["pred_labels"][pair[1]] == object_label:
+                            weight[idx] = 0
+            try:
+                at_loss = ce_loss(attention_distribution, attention_label)
+                losses["attention_relation_loss"] += (at_loss * weight).mean()
+            except ValueError:
+                # If there is only one object in the last frame of the context, we need to unsqueeze the label
+                attention_label = attention_label.unsqueeze(0)
+                at_loss = ce_loss(attention_distribution, attention_label)
+                losses["attention_relation_loss"] += (at_loss * weight).mean()
+            
+            if not conf.bce_loss:
+                sp_loss = mlm_loss(spatial_distribution, spatial_label)
+                losses["spatial_relation_loss"] += (sp_loss * weight).mean()
+                con_loss = mlm_loss(contact_distribution, contact_label)
+                losses["contact_relation_loss"] += (con_loss * weight).mean()
+            else:
+                sp_loss = bce_loss(spatial_distribution, spatial_label)
+                losses["spatial_relation_loss"] += (sp_loss * weight).mean()
+                con_loss = bce_loss(contact_distribution, contact_label)
+                losses["contact_relation_loss"] += (con_loss * weight).mean()
+            context += 1
+            count += 1
         else:
-            spatial_label = torch.zeros([len(pred["spatial_gt"][context_end_idx:future_end_idx]), 6],
-                                        dtype=torch.float32).to(device=attention_distribution.device)
-            contact_label = torch.zeros([len(pred["contacting_gt"][context_end_idx:future_end_idx]), 17],
-                                        dtype=torch.float32).to(device=attention_distribution.device)
-            for i in range(len(pred["spatial_gt"][context_end_idx:future_end_idx])):
-                spatial_label[i, pred["spatial_gt"][context_end_idx:future_end_idx][i]] = 1
-                contact_label[i, pred["contacting_gt"][context_end_idx:future_end_idx][i]] = 1
-
-        context_boxes_idx = torch.where(entry["boxes"][:, 0] == context)[0][0]
-        context_excluding_last_frame_boxes_idx = torch.where(entry["boxes"][:, 0] == context - 1)[0][0]
-        future_boxes_idx = torch.where(entry["boxes"][:, 0] == context + future - 1)[0][-1]
-
-        if conf.mode == 'predcls':
-            context_last_frame_labels = set(
-                pred["labels"][context_excluding_last_frame_boxes_idx:context_boxes_idx].tolist())
-            future_labels = set(pred["labels"][context_boxes_idx:future_boxes_idx + 1].tolist())
-            context_labels = set(pred["labels"][:context_boxes_idx].tolist())
-        else:
-            context_last_frame_labels = set(
-                pred["pred_labels"][context_excluding_last_frame_boxes_idx:context_boxes_idx].tolist())
-            future_labels = set(pred["pred_labels"][context_boxes_idx:future_boxes_idx + 1].tolist())
-            context_labels = set(pred["pred_labels"][:context_boxes_idx].tolist())
-
-        appearing_object_labels = future_labels - context_last_frame_labels
-        disappearing_object_labels = context_labels - context_last_frame_labels
-        ignored_object_labels = appearing_object_labels.union(disappearing_object_labels)
-        ignored_object_labels = list(ignored_object_labels)
-
-        # Weighting loss based on appearance or disappearance of objects
-        # We only consider loss on objects that are present in the last frame of the context
-        weight = torch.ones(pred["output"][count]["global_output"].shape[0]).cuda()
-        for object_label in ignored_object_labels:
-            for idx, pair in enumerate(pred["pair_idx"][context_end_idx:future_end_idx]):
-                if conf.mode == 'predcls':
-                    if pred["labels"][pair[1]] == object_label:
-                        weight[idx] = 0
-                else:
-                    if pred["pred_labels"][pair[1]] == object_label:
-                        weight[idx] = 0
-        try:
-            at_loss = ce_loss(attention_distribution, attention_label)
-            losses["attention_relation_loss"] += (at_loss * weight).mean()
-        except ValueError:
-            attention_label = attention_label.unsqueeze(0)
-            at_loss = ce_loss(attention_distribution, attention_label)
-            losses["attention_relation_loss"] += (at_loss * weight).mean()
-
-        if not conf.bce_loss:
-            sp_loss = mlm_loss(spatial_distribution, spatial_label)
-            losses["spatial_relation_loss"] += (sp_loss * weight).mean()
-            con_loss = mlm_loss(contact_distribution, contact_label)
-            losses["contact_relation_loss"] += (con_loss * weight).mean()
-        else:
-            sp_loss = bce_loss(spatial_distribution, spatial_label)
-            losses["spatial_relation_loss"] += (sp_loss * weight).mean()
-            con_loss = bce_loss(contact_distribution, contact_label)
-            losses["contact_relation_loss"] += (con_loss * weight).mean()
-
-        context += 1
-        count += 1
-
+            context += 1
+            count += 1
+            
     losses["attention_relation_loss"] = losses["attention_relation_loss"] / count
     losses["spatial_relation_loss"] = losses["spatial_relation_loss"] / count
     losses["contact_relation_loss"] = losses["contact_relation_loss"] / count
@@ -137,34 +134,33 @@ def process_train_video(conf, entry, optimizer, model, epoch, num, tr, gpu_devic
     loss.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5, norm_type=2)
     optimizer.step()
-
     num += 1
-
+    
     if num % 50 == 0:
         print("epoch {:2d}  batch {:5d}/{:5d}  loss {:.4f}".format(epoch, num, len(dataloader_train), loss.item()))
-
+    
     tr.append(pd.Series({x: y.item() for x, y in losses.items()}))
     if num % 1000 == 0 and num >= 1000:
         time_per_batch = (time.time() - start_time) / 1000
         print("\ne{:2d}  b{:5d}/{:5d}  {:.3f}s/batch, {:.1f}m/epoch".format(epoch, num, len(dataloader_train),
                                                                             time_per_batch,
                                                                             len(dataloader_train) * time_per_batch / 60))
-
+        
         mn = pd.concat(tr[-1000:], axis=1).mean(1)
         print(mn)
-
+    
     return num
 
 
 def process_test_video(conf, entry, model, gt_annotation, evaluator):
     get_sequence_no_tracking(entry, conf.mode)
     pred = model(entry, conf.baseline_context, conf.baseline_future)
-
+    
     count = 0
     context = conf.baseline_context
     future = conf.baseline_future
     total_frames = len(entry["im_idx"].unique())
-
+    
     context = min(context, total_frames - 1)
     future = min(future, total_frames - context)
     while context + 1 <= total_frames:
@@ -172,17 +168,17 @@ def process_test_video(conf, entry, model, gt_annotation, evaluator):
         prev_con = entry["im_idx"].unique()[context - 1]
         if context + future > total_frames > context:
             future = total_frames - context
-
+        
         future_frame_end_id = entry["im_idx"].unique()[context + future - 1]
         context_end_idx = int(torch.where(entry["im_idx"] == future_frame_start_id)[0][0])
         future_end_idx = int(torch.where(entry["im_idx"] == future_frame_end_id)[0][-1]) + 1
         entry_future_idx = entry["im_idx"][context_end_idx:future_end_idx]
         gt_future = gt_annotation[context:context + future]
-
+        
         context_boxes_idx = torch.where(entry["boxes"][:, 0] == future_frame_start_id)[0][0]
         context_excluding_last_frame_boxes_idx = torch.where(entry["boxes"][:, 0] == prev_con)[0][0]
         future_boxes_idx = torch.where(entry["boxes"][:, 0] == future_frame_end_id)[0][-1]
-
+        
         if conf.mode == 'predcls':
             context_last_frame_labels = set(
                 pred["labels"][context_excluding_last_frame_boxes_idx:context_boxes_idx].tolist())
@@ -193,19 +189,19 @@ def process_test_video(conf, entry, model, gt_annotation, evaluator):
                 pred["pred_labels"][context_excluding_last_frame_boxes_idx:context].tolist())
             future_labels = set(pred["pred_labels"][context:future_boxes_idx + 1].tolist())
             context_labels = set(pred["pred_labels"][:context].tolist())
-
+        
         box_mask = torch.ones(pred["boxes"][context_boxes_idx:future_boxes_idx + 1].shape[0])
         frame_mask = torch.ones(entry_future_idx.shape[0])
-
+        
         pred_future_im_idx = pred["im_idx"][context_end_idx:future_end_idx]
         pred_future_im_idx = pred_future_im_idx - pred_future_im_idx.min()
-
+        
         pair_idx = pred["pair_idx"][context_end_idx:future_end_idx]
         reshape_pair = pair_idx.view(-1, 2)
         min_value = reshape_pair.min()
         new_pair = reshape_pair - min_value
         pair_idx = new_pair.view(pair_idx.size())
-
+        
         appearing_objects_labels = future_labels - context_last_frame_labels
         disappearing_objects_labels = context_labels - context_last_frame_labels
         ignored_objects_labels = appearing_objects_labels.union(disappearing_objects_labels)
@@ -220,11 +216,11 @@ def process_test_video(conf, entry, model, gt_annotation, evaluator):
                     if pred["pred_labels"][pair[1]] == object_label:
                         frame_mask[idx] = 0
                         box_mask[pair_idx[idx, 1]] = 0
-
+        
         pred_future_im_idx = pred_future_im_idx[frame_mask == 1]
         removed = pair_idx[frame_mask == 0]
         mask_pair_idx = pair_idx[frame_mask == 1]
-
+        
         flat_pair = mask_pair_idx.view(-1)
         flat_pair_copy = mask_pair_idx.view(-1).detach().clone()
         for pair in removed:
@@ -232,16 +228,16 @@ def process_test_video(conf, entry, model, gt_annotation, evaluator):
             for i, p in enumerate(flat_pair_copy):
                 if p > idx:
                     flat_pair[i] -= 1
-
+        
         future_pair_idx = flat_pair.view(mask_pair_idx.size())
-
+        
         future_boxes = pred["boxes"][context_boxes_idx:future_boxes_idx + 1]
         future_labels = pred["labels"][context_boxes_idx:future_boxes_idx + 1]
         future_pred_labels = pred["pred_labels"][context_boxes_idx:future_boxes_idx + 1]
         future_scores = pred["scores"][context_boxes_idx:future_boxes_idx + 1]
         if conf.mode != 'predcls':
             future_pred_scores = pred["pred_scores"][context_boxes_idx:future_boxes_idx + 1]
-
+        
         if conf.mode == 'predcls':
             future_scores = future_scores[box_mask == 1]
             future_labels = future_labels[box_mask == 1]
@@ -249,7 +245,7 @@ def process_test_video(conf, entry, model, gt_annotation, evaluator):
             future_boxes = future_boxes[box_mask == 1]
         if conf.mode != 'predcls':
             future_pred_scores = future_pred_scores[box_mask == 1]
-
+        
         pred_dict = {
             'attention_distribution': pred["output"][count]['attention_distribution'][frame_mask == 1],
             'spatial_distribution': pred["output"][count]['spatial_distribution'][frame_mask == 1],
@@ -258,14 +254,14 @@ def process_test_video(conf, entry, model, gt_annotation, evaluator):
             'pair_idx': future_pair_idx,
             'im_idx': pred_future_im_idx,
         }
-
+        
         if conf.mode == 'predcls':
             pred_dict['labels'] = future_labels
             pred_dict['scores'] = future_scores
         else:
             pred_dict['pred_labels'] = future_pred_labels
             pred_dict['pred_scores'] = future_pred_scores
-
+        
         evaluator.evaluate_scene_graph(gt_future, pred_dict)
         count += 1
         context += 1
